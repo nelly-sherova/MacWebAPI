37260f5e346196341376c3bc4fe9ceece916f021
MM\nilufar <nilufar@mail.com>
2024-12-04 13:33:44 +0500

    Moving City Model to Data folder

--- MacWebApi/Data/Models/City.cs
+++ MacWebApi/Data/Models/City.cs
@@ -1,8 +1,8 @@
-﻿namespace MacWebApi.Models
+﻿namespace MacWebApi.Data.Models
 {
     public class City
     {
-        public int Id { get; set; } 
+        public int Id { get; set; }
         public string Name { get; set; }
         public string? Description { get; set; }
         public ICollection<Order> Orders { get; set; }
