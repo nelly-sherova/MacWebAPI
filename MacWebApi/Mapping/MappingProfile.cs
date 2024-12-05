9e2f1e8069b515f9bfb19a82a01477de18cf331b
MM\nilufar <nilufar@mail.com>
2024-12-05 10:53:14 +0500

    Add mapping

---
+++ MacWebApi/Mapping/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MacWebApi.Data.Models;
+using MacWebApi.DTOs;
+
+namespace MacWebApi.Mapping
+{
+    public class MappingProfile : Profile 
+    {
+        public MappingProfile() 
+        {
+            CreateMap<Category, CategoryDto>(); 
+            CreateMap<City, CityDto>();
+            CreateMap<Order, OrderDto>();
+            CreateMap<Product, ProductDto>();   
+            CreateMap<Role, RoleDto>();
+            CreateMap<User, UserDto>(); 
+        }
+    }
+}
--- MacWebApi/Program.cs
+++ MacWebApi/Program.cs
@@ -1,5 +1,8 @@
 using MacWebApi.Data;
+using MacWebApi.Mapping;
 using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +12,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
